bd03d1cd5f72eaa8e84c902359ad800e5b09e559
Neil Yonzon 2 <neilyonzon2@Neils-MacBook-Pro.local>
2019-08-05 19:58:23 -0700

    random commit

--- Sorting Algorithms/sortingAlgorithms.js
+++ Sorting Algorithms/sortingAlgorithms.js
@@ -11,6 +11,20 @@ const sortingAlgorithms = {
       }
     }
     return arr;
+  },
+  selectSort: arr => {
+    for (let i = 0; i < arr.length; i++) {
+      let min = i;
+      for (let j = i + 1; j < arr.length; j++) {
+        if (arr[j] < arr[i]) {
+          min = j;
+        }
+      }
+      let temp = arr[i];
+      arr[i] = arr[min];
+      arr[min] = temp;
+    }
+    return arr;
   }
 };
 
--- Sorting Algorithms/sortingAlgorithms.test.js
+++ Sorting Algorithms/sortingAlgorithms.test.js
@@ -10,3 +10,14 @@ test("Bubble sorts the array [2,0,1,4,6,5]", () => {
     6
   ]);
 });
+
+test("Selection sorts the array [2,0,1,4,5,3]", () => {
+  expect(sortingAlgorithms.selectSort([2, 0, 1, 4, 5, 3])).toEqual([
+    0,
+    1,
+    2,
+    3,
+    4,
+    5
+  ]);
+});
